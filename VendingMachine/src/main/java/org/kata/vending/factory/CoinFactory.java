7b9196ddd8e11fae357e0eb8a6de953ac40c28c4
Tgsikes <tgsikes@gmail.com>
2015-05-09 16:57:29 -0400

    Refactored coin test

--- VendingMachine/src/main/java/org/kata/vending/factory/CoinFactory.java
+++ VendingMachine/src/main/java/org/kata/vending/factory/CoinFactory.java
@@ -6,7 +6,7 @@ public class CoinFactory {
 
 	public static Coin create(double weight, double diameter, double thickness) {
 		Coin aCoin = null;
-		if (PennyCoin.weightTolerance(weight) && PennyCoin.diaTolerance(diameter) && PennyCoin.thicknessTolerance(thickness))
+		if (PennyCoin.inTolerance(weight,diameter,thickness))
 		aCoin = new PennyCoin();
 		return aCoin;
 	}
--- VendingMachine/src/test/java/org/kata/vending/factory/CoinFactoryTest.java
+++ VendingMachine/src/test/java/org/kata/vending/factory/CoinFactoryTest.java
@@ -8,7 +8,7 @@ import org.kata.vending.busObjs.Coin;
 public class CoinFactoryTest {
 
 	@Test
-	public void FactoryReturnsCoin() {
+	public void FactoryReturnsPenny() {
 		Coin coin = CoinFactory.create(2.5, .75, 1.52);
 		assertEquals(PennyCoin.class, coin.getClass());
 	}
